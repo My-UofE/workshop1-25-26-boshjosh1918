800fe857dbed3d19080170c3304138fd0a936789
Joshua <67383578+boshjosh1918@users.noreply.github.com>
2026-01-21 04:12:17 +0000

    Added return value to process_guess()

--- game_functions.py
+++ game_functions.py
@@ -16,7 +16,8 @@ def check_higher_lower(current_val, next_val, user_input):
 def process_guess(letter, board, word):
     if letter not in word:
         print(f"Sorry, '{letter}' is not in the word")
-        return
+        return False
     for i,char in enumerate(word):
         if char == letter: board[i] = letter
     print(f"Well done! '{letter}' is in the word")
+    return True
